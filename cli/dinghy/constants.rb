ea3e1b434ad84e230273040130058bc7dd5530e4
Justin Morris <desk@pixelbloom.com>
2015-11-09 11:43:20 +1100

    Never hardcode the path for Homebrew
    Having issues running dinghy when using a non-standard homebrew install path (`/opt/boxen/homebrew` in my case).

--- cli/dinghy/constants.rb
+++ cli/dinghy/constants.rb
@@ -5,13 +5,13 @@ MEM_DEFAULT=2048
 CPU_DEFAULT=1
 DISK_DEFAULT=20_000
 
+BREW = Pathname.new(`brew --prefix`.strip)
+
 # makes local dev easier
 if $0 =~ /bin\/_dinghy_command/ || $0 =~ /rspec/
-  BREW = Pathname.new('/usr/local')
   DINGHY = Pathname.new(File.expand_path("../..", File.dirname(__FILE__)))
   VAR = Pathname.new(File.expand_path("../../local/var", File.dirname(__FILE__)))
 else
-  BREW = Pathname.new(`brew --prefix`.strip)
   DINGHY = BREW+"opt/dinghy"
   VAR = BREW+"var/dinghy"
 end
